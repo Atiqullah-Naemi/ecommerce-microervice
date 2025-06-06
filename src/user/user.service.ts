385abdc5985a8d8ed56b5cfc26c3c9f3b849765b
Atiqullah-Naemi <atiqullah.naemi21@gmail.com>
2025-06-06 15:25:43 +1000

    configuring kafkaf

---
+++ src/user/user.service.ts
@@ -0,0 +1,29 @@
+import { PrismaService } from './../prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    return this.prisma.user.create({ data: createUserDto });
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: string) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: string) {
+    return `This action removes a #${id} user`;
+  }
+}
